13d71bc422851860f72fa483ca1b1ccba5bfcbda
fanghornn <fanghornn@gmail.com>
2015-12-27 18:58:10 +0100

    configuration file for nodejs server

---
+++ config.js
@@ -0,0 +1,11 @@
+var config =  {};
+
+config.hostName = 'fanghornn.com';
+
+//Set port
+config.port = '1338';
+
+//set dev boolean value (if false, minified scripts will load)
+config.dev = true;
+
+module.exports = config;
