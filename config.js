04806ee2cb59cf1edf8bc37a04956cdaa761a2e1
fanghornn <fanghornn@gmail.com>
2015-12-28 23:32:21 +0100

    added front end mail sender parameters

--- config.js
+++ config.js
@@ -8,4 +8,10 @@ config.port = '1338';
 //set dev boolean value (if false, minified scripts will load)
 config.dev = true;
 
+//Set here your mail acount login (This is used for the web form which allow the user to send me mails from my website)
+config.mailLogin = '';
+
+//and your mail account password
+config.mailPassword = '';
+
 module.exports = config;
