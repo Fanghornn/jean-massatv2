c034288d22933ae295669776cb61347db1411fed
fanghornn <fanghornn@gmail.com>
2015-12-29 01:46:10 +0100

    added custom MailHandler Class which handle mails sending from the front end html form

---
+++ src/js/MailHandler.js
@@ -0,0 +1,76 @@
+/**
+ *	Class MailHandler
+ *
+ *	Handle mail sending from the web form
+ */
+(function(){
+
+	"use strict";
+
+	var MailHandler = {
+
+		mailRequest : null,
+
+		/**
+		 * [initialize]
+		 * @return {[undefined]} 		[just attach custom submit handler]
+		 */
+		initialize : function(){
+
+			//attach Web form mail sender handler
+			$('#vaguemestre').submit(function(event){
+
+				event.preventDefault();
+
+				//Multiple click handler
+				if(self.mailRequest){
+					self.mailRequest.abort();
+				}
+
+				self.mailRequest = $.ajax({
+					type: 'POST',
+					url: '/vaguemestre',
+					data: $(this).serialize(),
+					success:self.requestSuccessHandler,
+					error:self.requestErrorHandler
+				});
+
+			});
+
+		},
+
+		/**
+		 * [requestErrorHandler triggered on ajax mail error]
+		 * @param  {[object]} 		request 		[jQuery request Object]
+		 * @return {[undefined]}
+		 */
+		requestErrorHandler : function(request){
+	
+			if(request.statusText !== 'abort'){
+				alertify.notify('Oups !<br/><br/>Une erreur est survenue durant l\'envoi du mail...<br/><br/>Merci d\'essayer ultérieurement.', 'error', 5);
+			}
+	
+		},
+
+		/**
+		 * [requestSuccessHandler triggered on ajax mail success]
+		 * @param  {[string]} 		res 			[Server response]
+		 * @return {[undefined]}
+		 */
+		requestSuccessHandler : function(res){
+		
+			if(res === 'ok'){
+				alertify.notify('Merci !<br/>j\'ai bien reçu votre mail.<br/>', 'success', 5);
+			}
+		
+		}
+
+	};
+
+	var self = MailHandler;
+
+	window.MailHandler = MailHandler;
+
+	MailHandler.initialize();
+
+})();
