539e646cb43f1d2e750c88cdce2ce04f6a59e4a4
fanghornn <fanghornn@gmail.com>
2016-01-01 22:51:07 +0100

    comment header on node scripts

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+/**
+ **************************************
+ *	@author: Jean Baptiste Priam Massat
+ *			 Aka Fanghornn
+ *			 
+ * 	@mail: jean.massat[at]gmail[dot]com
+ *
+ * 	@Project: My personnal website
+ *
+ *	@Copyright (c) <2014> <Jean Baptiste PRIAM MASSAT>
+ *
+ ************************************** 
+ */
 "use strict";
 
 var server = require('./libs/server.js'),
