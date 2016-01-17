57215a336d41ef75cea2129f25cbf8c4cf2c44ac
fanghornn <fanghornn@gmail.com>
2016-01-17 21:08:57 +0100

    Added indexof Object prototype method for old IE

--- src/js/site.js
+++ src/js/site.js
@@ -49,6 +49,33 @@
         $('html').addClass('desktop');
     }
 
+    /**
+     * Define indexOf for old ES
+     */
+    if (!Array.prototype.indexOf){
+        
+        Array.prototype.indexOf = function(elt /*, from*/){
+
+            var len = this.length >>> 0;
+            var from = Number(arguments[1]) || 0;
+            
+            from = (from < 0) ? Math.ceil(from) : Math.floor(from);
+            
+            if (from < 0){
+                from += len;
+            }
+
+            for (; from < len; from++){
+                if (from in this && this[from] === elt){
+                    return from;
+                }
+            }
+
+        return -1;
+
+        };
+
+    }
 
     /**
      * Functions
