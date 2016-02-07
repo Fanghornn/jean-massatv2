b3fdb84a218dc550ad3e03a191f5a93aa531a951
fanghornn <fanghornn@gmail.com>
2016-02-07 12:55:34 +0100

    Added cronjob pattern

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -2,6 +2,8 @@
 #
 service nginx stop  # We stop nginx server
 
+# crontab job:	 0 0 1 * * /path/to/ssl_renew.sh
+
 #Test command if ! /opt/letsencrypt/letsencrypt-auto certonly -tvv --standalone --renew-by-default -d jean-massat.com,www.jean-massat.com > /var/log/letsencrypt/renew.log 2>&1 ; then 
 if ! /opt/letsencrypt/letsencrypt-auto certonly -tvv --standalone --keep -d jean-massat.com,www.jean-massat.com > /var/log/letsencrypt/renew.log 2>&1 ; then
     echo ssl certif renew failed, please have a look on this log:
