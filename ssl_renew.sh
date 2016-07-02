6ed0e445b6d80a608024e70bc7b04c5f3b4642a0
fanghornn <fanghornn@gmail.com>
2016-07-02 18:30:47 +0200

    Adapting the renewal sh script for the new letsencrypt client

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,13 +4,13 @@ service nginx stop  # We stop nginx server
 
 # crontab job:	 0 0 1 * * /path/to/ssl_renew.sh
 
-#Test command if ! /opt/letsencrypt/letsencrypt-auto certonly -tvv --standalone --renew-by-default -d jean-massat.com,www.jean-massat.com > /var/log/letsencrypt/renew.log 2>&1 ; then 
-if ! /opt/letsencrypt/letsencrypt-auto certonly -tvv --standalone --keep -d jean-massat.com,www.jean-massat.com > /var/log/letsencrypt/renew.log 2>&1 ; then
-    echo ssl certif renew failed, please have a look on this log:
+#Test command if ! certbot certonly -tvv --standalone --renew-by-default -d jean-massat.com,www.jean-massat.com > /var/log/letsencrypt/renew.log 2>&1 ; then 
+if ! certbot certonly -tvv --standalone --keep -d jean-massat.com,www.jean-massat.com > /var/log/letsencrypt/renew.log 2>&1 ; then
+    echo "ssl certif renew failed, please have a look at the following log file:"
     cat /var/log/letsencrypt/renew.log
-    service nginx start #Restart nginx server
+    service nginx restart #Restart nginx server
     exit 1
 fi
 
-service nginx start #Restart nginx server
-echo sll certif renew success
+service nginx restart #Restart nginx server
+echo "sll certif renew success"
