b72dd19f8fa7716f5d976f7e4d6e6ed32934c3a8
Fanghornn <fanghornn@gmail.com>
2017-08-14 17:47:47 +0200

    execution permission for ssl renew script

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,16 +1,2 @@
 #!/bin/sh
-#
-service nginx stop  # We stop nginx server
-
-# crontab job:	 0 0 1 * * /path/to/ssl_renew.sh
-
-#Test command if ! certbot certonly -tvv --standalone --renew-by-default -d jean-massat.com,www.jean-massat.com > /var/log/letsencrypt/renew.log 2>&1 ; then 
-if ! certbot certonly -tvv --standalone --keep -d jean-massat.com,www.jean-massat.com > /var/log/letsencrypt/renew.log 2>&1 ; then
-    echo "ssl certif renew failed, please have a look at the following log file:"
-    cat /var/log/letsencrypt/renew.log
-    service nginx restart #Restart nginx server
-    exit 1
-fi
-
-service nginx restart #Restart nginx server
-echo "sll certif renew success"
+certbot renew --pre-hook "service nginx stop" --post-hook "service nginx start"
